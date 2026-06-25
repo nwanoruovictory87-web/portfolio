f433aca9b49f5ab67d9403f15338b48b747ed958
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-06-25 21:08:59 +0200

    created ovarol  menu section for moblie and ipad

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,7 @@
 function Menu() {
   return (
-    <div className="w-[50%] h-full right-0 absolute">
-      <div className="bg-black flex flex-col gap-4 p-5 w-full text-gray-200 text-[24px]">
+    <div className="w-[50%] min-[800px]:hidden max-w-69.25 h-full right-0 absolute">
+      <div className="bg-black flex flex-col gap-4 p-5 w-full text-gray-200 text-[24px] min-[500px]:rounded-2xl">
         <span className=" text-[#0505a0] cursor-pointer">
           <h5>Home</h5>
         </span>
@@ -16,6 +16,23 @@ function Menu() {
         </span>
         <span className="  cursor-pointer">
           <h5>Contact</h5>
+        </span>
+        {/**socials */}
+        <span className="flex gap-2 cursor-pointer items-center ">
+          <i className="fa-brands fa-github text-gray-200"></i>
+          <h5>github</h5>
+        </span>
+        <span className="flex gap-2 cursor-pointer items-center">
+          <i className="fa-brands fa-linkedin text-blue-400"></i>
+          <h5>linkedin</h5>
+        </span>
+        <span className="flex gap-2 cursor-pointer items-center">
+          <i className="fa-brands fa-youtube text-red-600"></i>
+          <h5>youtube</h5>
+        </span>
+        <span className="flex gap-2 cursor-pointer items-center">
+          <i className="fa-brands fa-facebook text-blue-600"></i>
+          <h5>facebook</h5>
         </span>
       </div>
     </div>
